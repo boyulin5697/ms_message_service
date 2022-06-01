f293dcf7a62e0f44e2c3e7a5b1053331c8c4d7e1
linby <linboyu@fjhb.cn>
2022-06-01 10:23:42 +0800

    update expose api levels

---
+++ ms_message_service_api/src/main/java/com/by/ms/message/service/api/apis/MessageApis.java
@@ -0,0 +1,16 @@
+package com.by.ms.message.service.api.apis;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+
+/**
+ * Message feign apis
+ * @author boyulin
+ * @since 2022/6/1
+ */
+@Component
+@FeignClient(value = "ms_message_service")
+public class MessageApis {
+
+
+}
