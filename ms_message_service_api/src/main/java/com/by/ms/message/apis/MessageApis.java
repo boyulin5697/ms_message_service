4df6a10b591cc3ccf9d77f1415a2c26327fdb69e
boyulin <1404916254@qq.com>
2022-06-02 01:20:50 +0800

    Modify and implement rpc

---
+++ ms_message_service_api/src/main/java/com/by/ms/message/apis/MessageApis.java
@@ -0,0 +1,38 @@
+package com.by.ms.message.apis;
+
+import com.by.ms.message.service.api.requests.SendMessageRequest;
+import com.by.ms.message.service.api.responses.SendMessageResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Message feign apis
+ * @author boyulin
+ * @since 2022/6/1
+ */
+@Component
+@FeignClient(value = "ms_message_service")
+public interface MessageApis {
+
+    /**
+     * Send register email
+     * @param request request
+     * @return response response
+     */
+    @RequestMapping(value = "/rpc/requestRegisterEmail",method = RequestMethod.POST)
+    SendMessageResponse<String> requestRegisterEmail(SendMessageRequest request);
+
+
+    /**
+     * Send register sms
+     * @param request request
+     * @return response response
+     */
+    @RequestMapping(value = "/rpc/request/requestRegisterSms",method = RequestMethod.POST)
+    SendMessageResponse<String> requestRegisterSms(SendMessageRequest request);
+
+
+
+}
